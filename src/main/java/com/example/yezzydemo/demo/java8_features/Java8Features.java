6839909d2ec95c3561020854f7f646315fc4b3ae
Yezzy <1062271698@qq.com>
2023-09-19 20:04:21 +0800

    dev

--- src/main/java/com/example/yezzydemo/demo/java8_features/Java8Features.java
+++ src/main/java/com/example/yezzydemo/demo/java8_features/Java8Features.java
@@ -1,14 +1,21 @@
 package com.example.yezzydemo.demo.java8_features;
 
-public class InterfaceDefault {
+public class Java8Features {
+
     public static void main(String[] args) {
+        interfaceDefault();
+    }
+
+    public static void interfaceDefault(){
         Formula formula = new Formula() {
             @Override
             public double sqrt(int x) {
                 return Math.sqrt(x);
             }
         };
-        System.out.println(formula.add(1,2));
-        System.out.println(formula.sqrt(4));
+        System.out.println("The res of default method:" + formula.add(1,1));
+        System.out.println("The res of designed method:" + formula.sqrt(16));
+
     }
+
 }
--- src/main/java/com/example/yezzydemo/demo/java8_features/Stream.java
+++
@@ -1,4 +0,0 @@
-package com.example.yezzydemo.demo.java8_features;
-
-public class Stream {
-}
