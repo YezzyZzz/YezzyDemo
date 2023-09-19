9b53749107cd41ab90cdfd95d9a5d2929584e42c
Yezzy <1062271698@qq.com>
2023-09-19 19:16:18 +0800

    first commit

---
+++ src/main/java/com/example/yezzydemo/YezzyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.yezzydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YezzyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YezzyDemoApplication.class, args);
+    }
+
+}
